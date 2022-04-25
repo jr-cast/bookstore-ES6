b9094206f227b605022ae921846a54f8add4c029
jr-cast <castanos@pm.me>
2022-04-25 12:44:08 -0500

    Modules implemented & refactor methods and functions according es6

---
+++ index.js
@@ -0,0 +1,43 @@
+// Import main classes; BooksRack, Book
+import { BooksRack, Book } from "./modules/mainClasses.js";
+
+// Import AddNewBook Function 
+import { addNewBook } from "./modules/addBookFunc.js";
+
+// Import DisplayElements Function
+import { displayElements } from "./modules/displayElements.js";
+
+// Import anchors functions
+import { checkListFunc, addNewFunc, contactFunc } from "./modules/anchorFunctions.js"
+
+// Populate local storage collection with user entries
+const storedBooks = new BooksRack();
+storedBooks.UpdateRack();
+addBtn.addEventListener('click', addNewBook);
+
+// Display localStorage Books Collection entries
+displayElements();
+
+// Remove books from collection and display
+for (let i = 0; i < document.getElementsByClassName('rmv').length; i += 1) {
+  const rmvButton = document.getElementById(`btn${i}`);
+  const titleName = storedBooks.Books[i].title;
+  const list = document.getElementById(`${i}`);
+  rmvButton.addEventListener('click', () => {
+    const filtered = storedBooks.Books.filter((Books) => Books.title !== titleName);
+    const stringData = JSON.stringify(filtered);
+    localStorage.setItem('Books', stringData);
+    list.remove();
+    window.location.reload();
+  });
+}
+
+// Anchors in Nav added functionality
+document.getElementById('list').addEventListener('click', checkListFunc);
+document.getElementById('new').addEventListener('click', addNewFunc);
+document.getElementById('cont').addEventListener('click', contactFunc);
+
+// Date
+const date = new Date();
+document.getElementById('date').innerHTML = date;
+
---
+++ modules/addBookFunc.js
@@ -0,0 +1,42 @@
+export class BooksRack { /* eslint-disable-line max-classes-per-file */
+  constructor() { /* eslint-disable-line no-unused-vars */
+    this.Books = [];
+  }
+
+  UpdateRack() {
+    if (localStorage.Books) {
+      this.Books = JSON.parse(localStorage.Books);
+    }
+  }
+}
+
+export class Book { /* eslint-disable-line max-classes-per-file */
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+const addBtn = document.getElementById('addBtn');
+const bookTitle = document.getElementById('title');
+const author = document.getElementById('author');
+const storedBooks = new BooksRack();
+storedBooks.UpdateRack();
+let stringData = JSON.stringify(storedBooks.Books);
+let book;
+
+export function addNewBook() {
+  if (!bookTitle.value || !author.value) {
+    alert('Enter both book title and author please');
+    // Display localStorage Books Collection entries
+  } else {
+    book = new Book(bookTitle.value, author.value);
+    storedBooks.Books.push(book);
+    bookTitle.value = '';
+    author.value = '';
+    stringData = JSON.stringify(storedBooks.Books);
+    localStorage.setItem('Books', stringData);
+    window.location.reload();
+  }
+};
+
